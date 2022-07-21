532a0e54874f59762f1ec64811f4689679daaf6e
Jessica <aleejess@gmail.com>
2022-07-20 21:56:26 -0500

    feat:added back button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
-import Home from './pages/Home';
+// import Home from './pages/Home';
+// import Loginform from './components/Log-In-form'
+import Signupform from './components/Sign-In-form'
 
 
 function App() {
   return (
     <div className="App">     
-      <Home />    
+      {/* <Home />     */}
+      {/* <Loginform /> */}
+      <Signupform />
             
     </div>
   );
